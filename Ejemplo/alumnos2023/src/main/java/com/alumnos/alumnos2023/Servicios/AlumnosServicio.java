e545b0ca9ef521e12888fd0ef5aefc418a91f1c7
Sebastian G <resgirolminipcparts2014@gmail.com>
2023-02-11 20:33:34 -0300

    Actualizando ejercicio propio Spring1

--- Ejemplo/alumnos2023/src/main/java/com/alumnos/alumnos2023/Servicios/AlumnosServicio.java
+++ Ejemplo/alumnos2023/src/main/java/com/alumnos/alumnos2023/Servicios/AlumnosServicio.java
@@ -7,49 +7,106 @@ import com.alumnos.alumnos2023.Entidades.Materias;
 import com.alumnos.alumnos2023.Repositorios.AlumnosRepositorio;
 import com.alumnos.alumnos2023.Repositorios.EscuelasRepositorio;
 import com.alumnos.alumnos2023.Repositorios.MateriasRepositorio;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AlumnosServicio {
-    
+
     //Indicamos al servidor de aplicaciones que esta variable va a ser inicializada por el
     @Autowired //(Esto se conoce como inyeccion de dependencia)
     private AlumnosRepositorio alumnosrepositorio;
-    
+
     @Autowired
     private EscuelasRepositorio escuelasrepositorio;
-    
+
     @Autowired
     private MateriasRepositorio materiasrepositorio;
-    
-    
-    public void crearAlumnos(Integer idAlumnos,String nombre,Integer dni,String idEscuelas,String idMaterias) {
-        
+
+    //Si se aplica una accion y no se ejecuta lo esperado volvera a cargar la pagina
+    //Todo los metodos que modifiquen de forma permanente la base de datos deberan ser
+    //tratados con una "transactional"
+    @Transactional
+    public void crearAlumnos(Integer idAlumnos, String nombre, Integer dni, String idEscuelas, String idMaterias) {
+
         Escuelas escuelas = escuelasrepositorio.findById(idEscuelas).get();
         Materias materias = materiasrepositorio.findById(idMaterias).get();
 
-
         //Primero que nada creamos un objeto
         Alumnos alumnos = new Alumnos();
-        
+
         // Luego de tener todos los atributos completo podemos guardar todo lo que cargue y lo persista luego a la base
         //de datos
-        
-        //Video Servicio 1 min 7.50
-        
+        //Setteamos
         alumnos.setIdAlumnos(idAlumnos);
         alumnos.setNombre(nombre);
         alumnos.setDni(dni);
         alumnos.setAlta(new Date());
-        
+
         alumnos.setEscuelas(escuelas);
         alumnos.setMaterias(materias);
+        //Persistimos
         alumnosrepositorio.save(alumnos);
-        
-        
-       
+
     }
-    
+
+    //Creamos una Lista para guardar todo los alumnos que encontremos
+    public List<Alumnos> listarAlumnos() {
+
+        List<Alumnos> alumnos = new ArrayList();
+
+        alumnos = alumnosrepositorio.findAll();
+
+        return alumnos;
+    }
+
+    //Metodo Modificar Alumnos
+    //Explicacion breve en el video 3 Servicios en el min 7.49
+    public void modificarAlumnos(Integer idAlumnos, String nombre, Integer dni, String idEscuelas, String idMaterias) {
+        //El Optional puede o no contener un valor no nulo, si el valor no es nulo
+        //se podra retornar por medio de un get ese valor
+        Optional<Alumnos> respuesta = alumnosrepositorio.findById(idAlumnos);
+        
+        Optional<Escuelas> respuestaEscuelas = escuelasrepositorio.findById(idEscuelas);
+        
+        Optional<Materias> respuestaMaterias = materiasrepositorio.findById(idMaterias);
+        
+        
+        //Instanciamos Escuelas y Materias
+        Escuelas escuelas = new Escuelas();
+        Materias materias = new Materias();
+        
+       if (respuestaEscuelas.isPresent()){
+           
+           escuelas = respuestaEscuelas.get();
+       }
+       
+       if (respuestaMaterias.isPresent()){
+           
+           materias = respuestaMaterias.get();
+       }
+
+        //Si la respuesta esta precente
+        if (respuesta.isPresent()) {
+        
+        //Nos retornara por medio del get lo que buscamos
+        Alumnos alumnos = respuesta.get();
+        
+        alumnos.setNombre(nombre);
+        alumnos.setEscuelas(escuelas);
+        alumnos.setMaterias(materias);
+        alumnos.setDni(dni);
+        //Ya teniendo todos los datos de los metodos creados como crear y modificar lo
+        //persistimo en nuestra base de datos
+        alumnosrepositorio.save(alumnos);
+       
+        }
+
+    }
+
 }
